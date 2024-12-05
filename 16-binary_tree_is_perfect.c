812fade07a48b007e23131e05dd5e7d5cc755a45
FionaGachuuri <fionairuka@gmail.com>
2024-12-05 08:31:58 +0300

    function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,27 @@
 #include "binary_trees.h"
 
+
+/**
+ * power - Computes the power of a number
+ * @base: Base number
+ * @exp: Exponent
+ * Return: base raised to the power exp
+ */
+
+size_t power(size_t base, size_t exp)
+{
+	size_t result = 1;
+
+	while (exp > 0)
+	{
+		result *= base;
+		exp--;
+	}
+
+	return (result);
+}
+
+
 /**
   * binary_tree_is_perfect -function that checks if
   * a binary tree is perfect
