366e46a8ded4e8ad6025e57d135429abdc5bcc75
FionaGachuuri <fionairuka@gmail.com>
2024-12-05 17:30:55 +0300

    checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,47 +1,40 @@
 #include "binary_trees.h"
 
-
 /**
- * power - Computes the power of a number
- * @base: Base number
- * @exp: Exponent
- * Return: base raised to the power exp
- */
-
-size_t power(size_t base, size_t exp)
-{
-	size_t result = 1;
-
-	while (exp > 0)
-	{
-		result *= base;
-		exp--;
-	}
-
-	return (result);
-}
-
-
-/**
-  * binary_tree_is_perfect -function that checks if
-  * a binary tree is perfect
-  * @tree:  pointer to the root node of the tree to check
-  * Return: 0 if tree is NULL and 1 if perfect
+  * binary_tree_is_perfect -checks if a binary tree is perfect
+  * @tree: pointer to the root node of the tree to check
+  * Return: height of tree, but 0 if tree is NULL
   */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height, size, expected_size;
+
+	size_t left_height, right_height;
+
 
 	if (tree == NULL)
 		return (0);
 
-	height = binary_tree_height(tree) - 1;
-	size = binary_tree_size(tree);
-	expected_size = power(2, height + 1) - 1;
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	else
+		left_height = 0;
 
-	return (size == expected_size);
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+	else
+		right_height = 0;
+
+	return ((left_height > right_height) ? left_height : right_height);
+
+	if (left_height != right_height)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	return (0);
 }
-
-
-
